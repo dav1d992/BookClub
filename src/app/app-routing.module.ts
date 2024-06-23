b399d3f8a6b6da4d510e9d64860be3bf52052a2d
David Lo <dav@danskecommodities.com>
2024-06-24 00:38:05 +0200

    Added pages + routing + initial home and about-us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { BlogsComponent } from './pages/blogs/blogs.component';
+import { BooksComponent } from './pages/books/books.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    children: [
+      {
+        path: 'about-us',
+        component: AboutUsComponent,
+      },
+      {
+        path: 'blogs',
+        component: BlogsComponent,
+      },
+      {
+        path: 'books',
+        component: BooksComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
