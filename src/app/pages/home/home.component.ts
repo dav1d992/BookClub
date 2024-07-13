976b111cbf9036853ba5fcd412ac60818b71a6d5
David Lo <dav@danskecommodities.com>
2024-07-13 15:51:07 +0200

    Added download option for al-irshad

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent {
+  private readonly http = inject(HttpClient);
 
-  ngOnInit() {}
+  downloadPdf(filename: string): void {
+    const fileUrl = `assets/${filename}`;
+    this.http.get(fileUrl, { responseType: 'blob' }).subscribe(
+      (response: Blob) => {
+        const url = window.URL.createObjectURL(response);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = filename;
+        a.click();
+        window.URL.revokeObjectURL(url);
+      },
+      (error) => {
+        console.error('Error downloading the file', error);
+      }
+    );
+  }
 }
