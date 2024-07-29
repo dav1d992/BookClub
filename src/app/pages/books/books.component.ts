c6e1f8639fb92d27546a9ffcf9f8b6c5783e2d57
David Lo <dav@danskecommodities.com>
2024-07-29 13:20:11 +0200

    Adjustment on order of books

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -17,6 +17,18 @@ export class BooksComponent {
       image: '../../../assets/al-mufid.png',
     },
     {
+      id: 'dusen-talker',
+      title: 'Tal med Selvsikkerhed',
+      date: '10-20-2023',
+      image: '../../../assets/images/dusen_speech.jpg',
+    },
+    {
+      id: 'zain-sailor',
+      title: 'Islam Overalt part II - Bølgerne',
+      date: '28-02-2024',
+      image: '../../../assets/images/zain_sails_boat.jpg',
+    },
+    {
       id: 'human-reader',
       title: 'Dyk ned i Litteraturen',
       date: '10-04-2023',
@@ -27,18 +39,6 @@ export class BooksComponent {
       title: 'Håndskrevne Tanker',
       date: '30-10-2024',
       image: '../../../assets/images/david_writes_letter.jpg',
-    },
-    {
-      id: 'dusen-talker',
-      title: 'Tal med Selvsikkerhed',
-      date: '10-20-2023',
-      image: '../../../assets/images/dusen_speech.jpg',
-    },
-    {
-      id: 'zain-sailor',
-      title: 'Frihed på Bølgerne',
-      date: '28-02-2024',
-      image: '../../../assets/images/zain_sails_boat.jpg',
     },
   ];
 
