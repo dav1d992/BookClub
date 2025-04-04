51b0d3c1ce7271028b9348f0d42708833f58a9fd
David Lo <dav@danskecommodities.com>
2025-04-04 22:35:13 +0200

    MOre updates

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,9 +1,18 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }), 
+        animate('1s', style({ opacity: 1 })), 
+      ]),
+    ]),
+  ],
 })
 export class AboutUsComponent implements OnInit {
   constructor() {}
